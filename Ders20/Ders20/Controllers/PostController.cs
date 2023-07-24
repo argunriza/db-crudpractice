911bfb24a37bd3a50f06e5148997272dd89dcd6e
argunriza <argunrizaa@gmail.com>
2023-07-24 21:47:36 +0300

    update

--- Ders20/Ders20/Controllers/PostController.cs
+++ Ders20/Ders20/Controllers/PostController.cs
@@ -11,9 +11,11 @@ namespace Ders20.Controllers
         {
             _dbContext = dbContext;
         }
+
         public IActionResult Index()
         {
             var posts = _dbContext.Posts.ToList();
+
             return View(posts);
         }
 
@@ -24,17 +26,57 @@ namespace Ders20.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromForm]Posts posts)
+        public IActionResult Create([FromForm] Posts post)
         {
             if (!ModelState.IsValid)
             {
                 ViewBag.Message = "Invalid Data!";
             }
 
-            _dbContext.Posts.Add(posts);
+            _dbContext.Posts.Add(post);
             _dbContext.SaveChanges();
-            ViewBag.Message = $"Product Created Successfully!{posts.Id}";
+            ViewBag.Message = $"Post Created Successfully!{post.Id}";
+
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update([FromRoute] int id)
+        {
+            Posts? dbEntity = _dbContext.Posts.SingleOrDefault(post => post.Id == id);
+
+            return View(dbEntity);
+        }
+
+        [HttpPost]
+        public IActionResult Update([FromRoute] int id, [FromForm] Posts post)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Message = "Invalid data!.";
+
+                return this.View();
+            }
+
+            Posts? dbEntity = this._dbContext.Posts.SingleOrDefault(post => post.Id == id);
+
+            if (dbEntity is null)
+            {
+                ViewBag.Message = "Product Not Found!";
+
+                return this.View();
+            }
+
+            dbEntity.Title = post.Title;
+            dbEntity.Content = post.Content;
+            dbEntity.CreatedAt = post.CreatedAt;
+            dbEntity.CreatedBy = post.CreatedBy;
+
+            this._dbContext.Posts.Update(dbEntity);
+            this._dbContext.SaveChanges();
+            ViewBag.Message = "Posts Updated Succesfully.";
+
+            return View();
         }
     }
 }
